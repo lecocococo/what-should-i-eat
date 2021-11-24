bcbe38f471e51f90dd9834b8e9b2c492dde960c0
lecocococo <chemwang13@naver.com>
2021-11-25 03:59:34 +0900

    Feat: 지도 부분 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./mapContainer";
+import React, { Component } from "react";
+import MapContainer from "./mapContainer";
 
+// class Map extends Component{
+//   render(){
+//     return(
+//       <div></div>
+//     );
+//   }
+// }
 function App() {
   return (
     <div className="App">
       <h1>점심뭐먹지?</h1>
+      <MapContainer></MapContainer>
     </div>
   );
 }
