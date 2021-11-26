0f4a9172a102ae31e2eb9a8485cde522ab2ec130
lecocococo <chemwang13@naver.com>
2021-11-27 03:00:21 +0900

    만드는 중

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "./mapContainer";
+import "./randomSelect";
 import React, { Component } from "react";
 import MapContainer from "./mapContainer";
+import { category } from "./randomSelect";
 
 // class Map extends Component{
 //   render(){
@@ -11,11 +13,27 @@ import MapContainer from "./mapContainer";
 //     );
 //   }
 // }
+
+class Food extends Component {
+  render() {
+    // 랜덤하게 하는것을 메서드화
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+    }
+
+    // 랜덤을 변수로
+    // let select = category[Math.floor(Math.random() * category.length)];
+    return <p>{category[getRandomInt(0, category.length)]}</p>;
+  }
+}
 function App() {
   return (
     <div className="App">
       <h1>점심뭐먹지?</h1>
-      <MapContainer></MapContainer>
+      <Food></Food>
+      {/* <MapContainer></MapContainer> */}
     </div>
   );
 }
