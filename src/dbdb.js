adf3ab0a03dc8fd71e0118c15b70ea888e93156e
lecocococo <chemwang13@naver.com>
2021-11-29 03:01:47 +0900

    axios install

---
+++ src/dbdb.js
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+
+const dbdb = () => {
+  return (
+    <div>
+      <button onClick={() => onDataHandler()}></button>
+    </div>
+  );
+};
+
+function onDataHandler(){
+    const axios
+}
