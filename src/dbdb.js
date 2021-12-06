413c5da3c3a5fb97488e94f7abe829d8be015243
lecocococo <chemwang13@naver.com>
2021-12-06 23:57:06 +0900

    Feat: 횟수 카운팅 추가

--- src/dbdb.js
+++ src/dbdb.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+// import LifeCount from "./life_count";
 
 class Dbdb extends Component {
   constructor(props) {
@@ -7,6 +8,7 @@ class Dbdb extends Component {
     this.state = {
       testbody: "",
       data: "",
+      life: 3,
     };
   }
 
@@ -33,6 +35,9 @@ class Dbdb extends Component {
       });
   };
 
+  // decrease = () => {
+  //   this.setState({ life: this.state.life - 1 });
+  // };
   onCall = () => {
     fetch("http://localhost:3001/api", {
       method: "post",
@@ -49,7 +54,11 @@ class Dbdb extends Component {
       this?.setState({
         data: d.category_name,
       });
-      // res.json();
+      this.setState({ life: this.state.life - 1 });
+      if (this.state.life === 0) {
+        alert("고민하지마세요!!!");
+        this.setState({ life: 3 });
+      }
     });
     // .then(async (json, d) => {
     //   console.log("1");
@@ -71,6 +80,11 @@ class Dbdb extends Component {
       <div>
         <h2>{this.state?.data}</h2>
         <button onClick={this.onCall}>bring</button>
+        <div>
+          <p>아직까지 고민하시나요?</p>
+          <p>계속돌려도 선택장애는 없어지지 않습니다!!!</p>
+          <p>{this.state.life}번 안에 나온걸로 먹으로 가세요!!</p>
+        </div>
       </div>
     );
   }
