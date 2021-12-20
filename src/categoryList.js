93c9cba9a4b7888a3ad1c9abe6af3cfca7d7bb58
lecocococo <chemwang13@naver.com>
2021-12-21 02:17:58 +0900

    Feat: 카테고리 선택시 제외된항목에 뜨도록 만들어줌

--- src/categoryList.js
+++ src/categoryList.js
@@ -1,7 +1,28 @@
 import React from "react";
 
+const onBanned = (e) => {
+  const ban = document.querySelector(".ban");
+
+  //   console.log(ban);
+  //   ban.innerHTML(<span>sdfsdf</span>);
+  console.log(e.target.id);
+  console.log(
+    (ban.innerHTML +=
+      '<span class="del_box">' +
+      e.target.id +
+      "<button>" +
+      "X" +
+      "</button>" +
+      "</span>")
+  );
+  return;
+};
 function Button({ id }) {
-  return <button id={id}>{id}</button>;
+  return (
+    <button id={id} onClick={onBanned}>
+      {id}
+    </button>
+  );
 }
 function ButtonList({ category }) {
   return (
