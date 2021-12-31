837b55c034cc22684487b5eeda9f29eb33ed1a05
lecocococo <chemwang13@naver.com>
2021-12-31 23:39:13 +0900

    Feat: 모달창이 떳을경우 로딩바를 다시 처음 상태로 되돌아 가도록 함 + 추가적인 css변경

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import "./mapContainer";
 import "./randomSelect";
 import React, { useState, useEffect, Component } from "react";
 import MapContainer from "./mapContainer";
-// import { category } from "./randomSelect";
 import LoadingSpinner from "./loadingSpinner";
 import "./dbdb";
-import Dbdb from "./dbdb";
 import Modal from "./modal";
-import modalCss from "./modal.module.css";
+import styles from "./styles.module.css";
 
 // redux사용
 import { useDispatch, useSelector } from "react-redux";
@@ -17,6 +15,7 @@ import { changeModalState } from "./reducer/modal_state";
 
 //react-spring 사용
 import { useSprings, animated, useSpring, config } from "react-spring";
+import useMeasure from "react-use-measure";
 
 // class Food extends Component {
 //   render() {
@@ -36,7 +35,8 @@ import { useSprings, animated, useSpring, config } from "react-spring";
 function App() {
   const [loading, setLoading] = useState(true);
   const [category_data, setCategory_data] = useState([]);
-  const styles = useSpring({
+  // react-spring
+  const style = useSpring({
     loop: { reverse: true },
     config: config.wobbly /*{ friction: 5 }*/,
     from: { rotateZ: -20 },
@@ -45,6 +45,16 @@ function App() {
     // to: { x: 100 },
     // reset: false,
   });
+
+  const [open, toggle] = useState(false);
+  const [ref, { width }] = useMeasure();
+  const prop = useSpring({
+    width: open ? width : 0,
+    backgroundColor: open ? "turquoise" : "lightblue",
+    config: { duration: 1000 },
+  });
+
+  //
 
   let list = [];
   const call = () =>
@@ -66,9 +76,7 @@ function App() {
       for (let i = 0; i < list_data.length; i++) {
         list[i] = list_data[i].category_name;
       }
-      console.log(list);
       setCategory_data(list);
-      console.log(category_data);
     });
 
   //모달창이 떳을경우 배경을 흐리고 어둡게 만들어 주는 함수
@@ -82,29 +90,36 @@ function App() {
     setTimeout(() => {
       call();
       setLoading(false);
-    }, 2000);
+    }, 1000);
   }, []);
   //redux를 이용하여 state저장
   const dispatch = useDispatch();
   const modal_state = useSelector((state) => state.modal.modal);
 
   function onCall(e) {
-    //모달창 상태를 저장
-    console.log(modal_state);
-    dispatch(changeModalState(modal_state));
-    console.log(modal_state);
-    handleBackground();
+    toggle(!open);
+    setTimeout(() => {
+      toggle(open);
+      //모달창 상태를 저장
+      dispatch(changeModalState(modal_state));
+      handleBackground();
+    }, 1100);
   }
   return loading ? (
     <LoadingSpinner />
   ) : (
     <div className="App">
-      <animated.div style={styles}>
+      <animated.div className={styles.title} style={style}>
         <h1>점심뭐먹지?</h1>
-        {/* <Dbdb></Dbdb> */}
       </animated.div>
       <button onClick={onCall}>START</button>
-      {modal_state ? <Modal category_data={category_data}></Modal> : ""}
+      <div ref={ref} className={styles.main} onClick={() => toggle(!open)}>
+        <animated.div className={styles.fill} style={prop} />
+        <animated.div className={styles.content}>
+          {prop.width.to((x) => Math.floor((x * 100) / width) + "%")}
+        </animated.div>
+        {modal_state ? <Modal category_data={category_data}></Modal> : ""}
+      </div>
     </div>
   );
 }
